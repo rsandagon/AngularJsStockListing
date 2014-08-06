004cca7f6892a86b4c7ca0da88f6b52e7e2d1242
Ryan S <rsandagon@gmail.com>
2014-08-06 07:37:01 +0000

    test setup

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularJsStockListingApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularJsStockListingApp
+ */
+angular.module('angularJsStockListingApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
